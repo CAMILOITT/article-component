08e6ffb5f4b3bd68ab93af0e4ad64c1636aacd9d
CAMILOITT <camilocamilo5@outlook.com>
2022-10-10 19:08:33 -0500

    fixing bug

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const $boxShare = document.getElementById('boxShare');
 
 window.addEventListener('click', (e) => {
   const clicked = e.target;
-  if (!clicked.classList.contains('footer__icon-active')) {
+  if (clicked.classList.contains('icon-active')) {
     $share.classList.toggle('footer__icon-active');
     $boxShare.classList.toggle('footer__share-option-active');
   } else {
