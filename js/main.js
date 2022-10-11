5c83798b0bbdd63aa9753c4957f4421f821f65ca
CAMILOITT <camilocamilo5@outlook.com>
2022-10-10 19:00:08 -0500

    change file js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const $share = document.getElementById('share');
 const $boxShare = document.getElementById('boxShare');
-const $body = document.querySelector('body');
 
-$share.addEventListener('click', (e) => {
-  $share.classList.toggle('footer__icon-active');
-  $boxShare.classList.toggle('footer__share-option-active');
-});
-
-$share.addEventListener('blur', (e) => {
-  $share.classList.remove('footer__icon-active');
-  $boxShare.classList.remove('footer__share-option-active');
+window.addEventListener('click', (e) => {
+  const clicked = e.target;
+  if (!clicked.classList.contains('footer__icon-active')) {
+    $share.classList.toggle('footer__icon-active');
+    $boxShare.classList.toggle('footer__share-option-active');
+  } else {
+    $share.classList.remove('footer__icon-active');
+    $boxShare.classList.remove('footer__share-option-active');
+  }
 });
